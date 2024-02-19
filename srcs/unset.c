8677fdd8f5caeff9062e11af5a9aea1399598bea
Yan Costode <ycostode@3F6.42angouleme.fr>
2024-02-19 16:23:47 +0100

    communication
    Took 1 hour 28 minutes

--- srcs/unset.c
+++ srcs/unset.c
@@ -24,61 +24,18 @@ size_t len_until_sign(char  *str, int sign)
     return (i);
 }
 
-void    free_envp(char  **envp)
-{
-    int i;
-
-    i = 0;
-    while (envp[i])
-    {
-        free(envp[i]);
-        i++;
-    }
-    free(envp);
-}
-
-char    **new_envp(char **envp, int to_avoid)
-{
-    char    **env;
-    int     i;
-    int     j;
-
-    env = ft_calloc(count_args(envp) - 1, sizeof(char *));
-    if (!env)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (envp[i])
-    {
-        if (i == to_avoid)
-            j++;
-        env[i + j] = ft_strdup(envp[i]);
-        i++;
-    }
-    env[i + j] = 0;
-    return (env);
-}
-
 void    unset(t_program *program, char *s)
 {
-    static int  j = 0;
     size_t i;
-    int     var;
 
     i = 0;
-    var = 0;
     while (program->envp[i])
     {
         if (!ft_strncmp_case(program->envp[i], s, ft_strlen(s)) && len_until_sign(program->envp[i], '=') == ft_strlen(s))
         {
-            var = i;
+            program->envp[i] = "";
             break ;
         }
         i++;
     }
-    if (j)
-        free_envp(program->envp);
-    j++;
-    if (var)
-        program->envp = new_envp(program->envp, var);
 }
