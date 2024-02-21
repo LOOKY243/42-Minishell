6affe45ccfc0728e518cd5a047221b5b03b758de
Gad Marre <gmarre@2F7.42angouleme.fr>
2024-02-21 15:52:26 +0100

    merge n 6728346487321647832

--- srcs/pipe1.c
+++ srcs/pipe1.c
@@ -6,7 +6,7 @@
 /*   By: gmarre <gmarre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:19:34 by ycostode          #+#    #+#             */
-/*   Updated: 2024/02/21 14:37:09 by gmarre           ###   ########.fr       */
+/*   Updated: 2024/02/21 15:51:15 by gmarre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,26 @@ int	exec(t_program *program, char *cmd)
 
 	if (pipe(program->pipe) == -1 || pipe(program->data_pipe) == -1)
 		return (print_error("\x1b[1;6;31mpipe", EXIT_FAILURE));
-	if (!is_recoded(cmd))
+	//if (!is_recoded(cmd))
+	pid = fork();
+	if (pid == -1)
+		return (print_error("\x1b[1;6;31mfork", EXIT_FAILURE));
+	program->pid[program->cmd.current] = pid;
+	if (pid == 0)
 	{
-		pid = fork();
-		if (pid == -1)
-			return (print_error("\x1b[1;6;31mfork", EXIT_FAILURE));
-		program->pid[program->cmd.current] = pid;
-		if (pid == 0)
-		{
+		if (!is_recoded(cmd))
 			treat_child(program, cmd, program->cmd.current, program->cmd.len - 1);
-			exit(EXIT_SUCCESS);
-		}
-		waitpid(pid, NULL, 0);
+		else
+			treat_child_no_fork(program, cmd, program->cmd.current, program->cmd.len - 1);
+		close(program->data_pipe[PIPE_READ]);
+		write(program->data_pipe[PIPE_WRITE], &program->envp, sizeof(char **));
+		close(program->data_pipe[PIPE_WRITE]);
+		exit(EXIT_SUCCESS);
 	}
-	else
-		treat_child_no_fork(program, cmd, program->cmd.current, program->cmd.len - 1);
+	waitpid(pid, NULL, 0);
+	close(program->data_pipe[PIPE_WRITE]);
+	read(program->data_pipe[PIPE_READ], program->envp, sizeof(char **));
+	close(program->data_pipe[PIPE_READ]);
 	if (program->pipe_saved != -1)
 		close(program->pipe_saved);
 	program->pipe_saved = program->pipe[PIPE_READ];
